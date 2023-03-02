bcf64337b304c35355e3c103567a13900ef90935
jishnukoliyadan <83589431+jishnukoliyadan@users.noreply.github.com>
2023-03-02 13:01:10 +0530

    Query 5 Solution

--- SQL_Query.sql
+++ SQL_Query.sql
@@ -29,22 +29,32 @@
 
 # ----------------------------------------------------------------------------------------------------------------------- #
 
-# 4. Follow-up: Which segment had the most increase in unique products in 2021 vs 2020?
-WITH fy20 AS(
-        SELECT segment, COUNT(DISTINCT(fm.product_code)) AS seg20 FROM fact_sales_monthly fm
-            JOIN dim_product dp
-            ON fm.product_code = dp.product_code
-            WHERE fiscal_year = 2020
-            GROUP BY dp.segment),
+-- # 4. Follow-up: Which segment had the most increase in unique products in 2021 vs 2020?
+-- WITH fy20 AS(
+--         SELECT segment, COUNT(DISTINCT(fm.product_code)) AS seg20 FROM fact_sales_monthly fm
+--             JOIN dim_product dp
+--             ON fm.product_code = dp.product_code
+--             WHERE fiscal_year = 2020
+--             GROUP BY dp.segment),
             
-    fy21 AS(
-        SELECT segment, COUNT(DISTINCT(fm.product_code)) AS seg21 FROM fact_sales_monthly fm
-            JOIN dim_product dp
-            ON fm.product_code = dp.product_code
-            WHERE fiscal_year = 2021
-            GROUP BY dp.segment)
+--     fy21 AS(
+--         SELECT segment, COUNT(DISTINCT(fm.product_code)) AS seg21 FROM fact_sales_monthly fm
+--             JOIN dim_product dp
+--             ON fm.product_code = dp.product_code
+--             WHERE fiscal_year = 2021
+--             GROUP BY dp.segment)
             
-SELECT fy20.segment, seg20 AS product_count_2020, seg21 AS product_count_2021, seg21-seg20 AS difference FROM fy20
-    JOIN fy21
-    ON fy20.segment = fy21.segment
-    ORDER BY difference DESC;
+-- SELECT fy20.segment, seg20 AS product_count_2020, seg21 AS product_count_2021, seg21-seg20 AS difference FROM fy20
+--     JOIN fy21
+--     ON fy20.segment = fy21.segment
+--     ORDER BY difference DESC;
+
+# ----------------------------------------------------------------------------------------------------------------------- #
+
+# 5. Get the products that have the highest and lowest manufacturing costs.
+SELECT fc.product_code, product, manufacturing_cost FROM fact_manufacturing_cost as fc
+    JOIN dim_product as dp
+    ON fc.product_code = dp.product_code
+    WHERE fc.manufacturing_cost = (SELECT max(manufacturing_cost) FROM fact_manufacturing_cost) OR
+        fc.manufacturing_cost = (SELECT min(manufacturing_cost) FROM fact_manufacturing_cost)
+    ORDER BY manufacturing_cost DESC;
